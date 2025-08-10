ab8b87d2f6664f7d304ced61217ebbd52f836aeb
vvvilla12 <vvillappaa@gmail.com>
2025-08-10 01:36:11 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~UrtUTC4D#1GOfW4YerfRYSciNPm7tOWPugUOIxxEPjRwpPz2urUI",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
